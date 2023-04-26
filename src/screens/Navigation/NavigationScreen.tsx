afca1563db5dbd3867cf63e07a689f06dbbe90f0
kyle1373 <59634395+kyle1373@users.noreply.github.com>
2023-04-26 08:04:08 -0700

    Navigation screen finished

--- src/screens/Navigation/NavigationScreen.tsx
+++ src/screens/Navigation/NavigationScreen.tsx
@@ -1,4 +1,13 @@
-import { StyleSheet, Text, Keyboard, TextInput, View, TouchableWithoutFeedback } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  TextInput,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React, { useContext, useEffect, useState } from "react";
 import MapView from "react-native-maps";
 import { COLORS, FONTS, ResortMap, SIZES } from "../../constants/constants";
@@ -7,11 +16,17 @@ import { getResortMap } from "../../services/ResortService";
 import { ResortContext } from "../../contexts/ResortContext";
 import { displayError } from "../../helpers/helpers";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
 
 const NavigationScreen = () => {
   const [currentMap, setCurrentMap] = useState<ResortMap>();
   const { setLoading } = useContext(ScreenContext);
   const [isRouting, setIsRouting] = useState(false);
+
+  const [easySelected, setEasySelected] = useState(false);
+  const [mediumSelected, setMediumSelected] = useState(false);
+  const [hardSelected, setHardSelected] = useState(false);
 
   const { currentResort } = useContext(ResortContext);
 
@@ -51,6 +66,65 @@ const NavigationScreen = () => {
           showsUserLocation={true}
           style={styles.map}
         />
+        <View style={styles.bottomBackdrop}>
+          <Text style={{ fontFamily: FONTS.Medium, fontSize: 20 }}>
+            Difficulty (select many)
+          </Text>
+          <View style={styles.buttonControlsContainer}>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  setEasySelected(!easySelected);
+                }}
+                style={{
+                  ...styles.difficultyButton,
+                  borderColor: easySelected ? COLORS.black : COLORS.white,
+                }}
+              >
+                <MaterialCommunityIcons name="circle" size={30} color="green" />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  setMediumSelected(!mediumSelected);
+                }}
+                style={{
+                  ...styles.difficultyButton,
+                  borderColor: mediumSelected ? COLORS.black : COLORS.white,
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="square"
+                  size={30}
+                  color="darkblue"
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  setHardSelected(!hardSelected);
+                }}
+                style={{
+                  ...styles.difficultyButton,
+                  borderColor: hardSelected ? COLORS.black : COLORS.white,
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="cards-diamond"
+                  size={30}
+                  color="black"
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.startButton}>
+                <Text style={styles.startButtonText}>Start</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -91,5 +165,57 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 3.84,
     elevation: 5,
-  }
+  },
+  bottomBackdrop: {
+    backgroundColor: COLORS.lightBlue,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    position: "absolute",
+    bottom: 0,
+    zIndex: 10,
+    left: 15,
+    right: 15,
+    shadowColor: COLORS.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 3.84,
+    elevation: 5,
+    padding: 20,
+    paddingBottom: SIZES.bottomBarHeight + 10,
+  },
+  startButton: {
+    backgroundColor: COLORS.blue,
+    borderRadius: SIZES.height,
+    marginLeft: 10,
+  },
+  difficultyButton: {
+    backgroundColor: COLORS.white,
+    padding: 10,
+    borderRadius: SIZES.height,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: 3,
+  },
+  startButtonText: {
+    fontFamily: FONTS.Medium,
+    fontSize: 16,
+    color: COLORS.white,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  buttonControlsContainer: {
+    flexDirection: "row",
+    marginTop: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
